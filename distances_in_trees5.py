03ca84b48a38216383e7d1214f61a7ea05f01160
tz2614 <tony_zheng35@hotmail.com>
2018-05-21 16:17:19 +0100

    distances_in_trees5.py

---
+++ distances_in_trees5.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+"""
+Problem: Distances in Trees
+URL: http://rosalind.info/problems/nwck/
+
+Given: A collection of n trees (n <= 40) in Newick format, with each tree
+containing at most 200 nodes; each tree Tk is followed by a pair of nodes xk 
+and yk in Tk.
+
+Return: A collection of n positive integers, for which the kth integer
+represents the distance between xk and yk in Tk.
+"""
+
+
+from __future__ import print_function
+import os
+import sys
+
+
+class Newick(object):
+    def __init__(self, clade, children, weight=1):
+        self.clade = clade
+        self.children = children
+        self.weight = weight
+
+    @classmethod
+    def parse(cls, text):
+        text = text.strip().rstrip(';')
+        if text.count('(') == 0:
+            return Newick.parse_clade(text)
+        close_posn = text.rfind(')')
+        subtrees = []
+        plevel = 0
+        prev = 1
+        for posn in range(1, close_posn):
+            if text[posn] == '(':
+                plevel += 1
+            elif text[posn] == ')':
+                plevel -= 1
+            elif text[posn] == ',' and plevel == 0:
+                subtrees.append(text[prev:posn])
+                prev = posn + 1
+        subtrees.append(text[prev:close_posn])
+        clade = Newick.parse_clade(text[close_posn + 1:])
+        clade.children = [Newick.parse(st) for st in subtrees]
+        return clade
+
+    @classmethod
+    def parse_clade(cls, text):
+        if ':' in text:
+            tag, weight = text.split(':')
+        else:
+            tag, weight = text, 1
+        return cls(tag, [], weight=int(weight))
+
+    def path(self, n1, path=[]):
+        if n1 == self.clade:
+            return path
+        if not self.children:
+            return None
+        for c in self.children:
+            result = c.path(n1, path + [c])
+            if result:
+                return result
+
+    def distance(self, n1, n2=None, weighted=False):
+        p1 = self.path(n1)
+        if n2 is None:
+            return len(p1)
+        p2 = self.path(n2)
+        same = 0
+        same_path = []
+        while same < min(len(p1), len(p2)) and p1[same] == p2[same]:
+            same_path.append(p1[same])
+            same += 1
+        return (sum(p.weight for p in p1) +
+                sum(p.weight for p in p2) -
+                2 * sum(s.weight for s in same_path))
+
+    def __repr__(self):
+        return self.clade
+
+
+def parse_file(ifile):
+    trees = []
+    nodes = []
+    running = True
+    while running:
+        line = ifile.readline().strip()
+        if line:
+            trees.append(Newick.parse(line))
+            nodes.append(ifile.readline().rstrip().split())
+            running = ifile.readline()
+        else:
+            running = False
+
+    return trees, nodes
+
+
+if __name__ == "__main__":
+    dataset = sys.argv[1]
+    with open(dataset) as data_file:
+        trees, nodes = parse_file(data_file)
+        print(*[int(t.distance(*n)) for t, n in zip(trees, nodes)])
